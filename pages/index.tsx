823d844e5ae4753c1c00ac3ec64c3226bd6f41d0
Kh-BatErdene <hadabagi85@gmail.com>
2024-04-16 19:09:56 +0800

    function done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,18 @@ import { ChangeEvent, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState<string[]>();
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (e.target.files) {
-      const file = e.target.files[0];
-      await handleUpload(file);
+      const fileList = Array.from(e.target.files);
+
+      const accessUrl = await handleMultipleUploads(fileList);
+      setSelectedFiles(accessUrl);
     }
   };
+
   const handleUpload = async (file: File) => {
     try {
       const response = await fetch("/api/upload", {
@@ -33,10 +36,36 @@ export default function Home() {
     }
   };
 
+  const handleMultipleUploads = async (fileList) => {
+    console.log(fileList);
+    if (!fileList) return;
+    const accessUrls: string[] = [];
+
+    await Promise.all(
+      fileList.map(async (file) => {
+        const accessUrl = await handleUpload(file);
+        if (accessUrl) {
+          accessUrls.push(accessUrl);
+        }
+      })
+    );
+
+    console.log("Uploaded files:", accessUrls);
+    return accessUrls;
+  };
+
   return (
     <div>
-      <input type="file" onChange={(e) => handleChange(e)} />
-      <img src={selectedFile} />
+      <input type="file" onChange={handleChange} multiple />
+      {selectedFiles ? (
+        selectedFiles.map((file: any, index) => (
+          <div key={index}>
+            <img src={file} alt={`File ${index}`} width="200" />
+          </div>
+        ))
+      ) : (
+        <div>No file</div>
+      )}
     </div>
   );
 }
